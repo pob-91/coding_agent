d66a2d2bb37d019a3f8d4c27bb2544e90acf940a
Rob Sanders <rob_sanders26@hotmail.co.uk>
2026-03-06 22:11:14 +0000

    migrate issue handler

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Any
+
+from model.webhook_message import WebhookMessage
 
 
 class BaseHandler(ABC):
     @abstractmethod
-    async def handle(self, data: Any) -> None:
+    async def handle(self, data: WebhookMessage) -> None:
         pass
